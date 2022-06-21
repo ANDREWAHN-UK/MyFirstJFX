2a74c656cc4a98aa3657a8c232e5c98e60dbc919
ANDREWAHN-UK <andrew.ahn@hotmail.co.uk>
2022-06-21 11:03:59 +0100

    Changed some text; Added an images folder; Added an image into the folder;

--- src/main/java/com/myfirstjfx/myfirstjfx/HelloApplication.java
+++ src/main/java/com/myfirstjfx/myfirstjfx/HelloApplication.java
@@ -4,6 +4,12 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import javafx.application.Application;
+import javafx.scene.Group;
 
 import java.io.IOException;
 
@@ -11,11 +17,12 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 320, 400);
+        stage.setTitle("A tool for students...");
         stage.setScene(scene);
         stage.show();
-    }
+
+        }
 
     public static void main(String[] args) {
         launch();
--- src/main/java/com/myfirstjfx/myfirstjfx/HelloController.java
+++ src/main/java/com/myfirstjfx/myfirstjfx/HelloController.java
@@ -1,14 +1,19 @@
 package com.myfirstjfx.myfirstjfx;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
+
+    @FXML
+    private Button btnWelcome;
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Welcome Students!");
+        btnWelcome.setText("This button was clicked.");
     }
 }
